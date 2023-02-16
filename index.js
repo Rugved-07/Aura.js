dbcbfa4bf00322e01bdaf177dc11f6ddaa29f5d3
Rugved-07 <125567475+Rugved-07@users.noreply.github.com>
2023-02-16 18:13:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const { Plugins } = require ("./src/index.js")
 
 
-console.log('\033[36mmidori.js is trying to be packed!');
+console.log('\033[36maura.js is trying to be packed!');
 
 
 console.log('\033[92mDiscord server : https://discord.gg/xPURT2B3sA'); setTimeout(() => { 
   
-console.log('\033[32mmidori.js is now sucessfully packed!'); }, 1500);
+console.log('\033[32maura.js is now sucessfully packed!'); }, 1500);
 
 
 module.exports = {
